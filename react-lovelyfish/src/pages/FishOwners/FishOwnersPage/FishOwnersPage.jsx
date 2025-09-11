ec560a8550bcf1a880532fe366dcfeeada94350d
Jinfeng-Zhang-1158659 <jinfengtrina@gmail.com>
2025-09-11 14:27:58 +1200

    fishowners updated

--- react-lovelyfish/src/pages/FishOwners/FishOwnersPage/FishOwnersPage.jsx
+++ react-lovelyfish/src/pages/FishOwners/FishOwnersPage/FishOwnersPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState }  from "react";
 import FishOwnerForm from "../FishOwnerForm/FishOwnerForm";
 import FishOwnersList from "../FishOwnersList/FishOwnersList";
 import "./FishOwnersPage.css";
