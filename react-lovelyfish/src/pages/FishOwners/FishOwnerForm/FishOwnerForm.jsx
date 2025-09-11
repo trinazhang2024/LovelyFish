6d8d290de931dd9c391554bf9e443e6e74d7af4b
Jinfeng-Zhang-1158659 <jinfengtrina@gmail.com>
2025-09-11 14:24:09 +1200

    fishownersform updated

--- react-lovelyfish/src/pages/FishOwners/FishOwnerForm/FishOwnerForm.jsx
+++ react-lovelyfish/src/pages/FishOwners/FishOwnerForm/FishOwnerForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import api from "../api/axios";
+import api from "../../../API/axios";
 import "./FishOwnerForm.css";
 
 function FishOwnerForm({ onAdded }) {
