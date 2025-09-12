f859830e364f2f51803a099cef1f6a081d68f883
Jinfeng-Zhang-1158659 <jinfengtrina@gmail.com>
2025-09-12 19:00:29 +1200

    ProductDetail.css updated

--- react-lovelyfish/src/pages/Search/SearchResultsPage.jsx
+++ react-lovelyfish/src/pages/Search/SearchResultsPage.jsx
@@ -38,10 +38,12 @@ const SearchResultsPage = () => {
 
   const keyword = normalize(query); // Normalized search keyword
 
+  console.log(products.items.map(p => normalize(p.categoryTitle)), "keyword:", keyword);
+
   // Filter products based on title, description, or category
   const filteredProducts = (products.items || []).filter(product => {
-    const title = normalize(product.title);
     
+    const title = normalize(product.title);
     const category = normalize(product.categoryTitle);
 
     return title.includes(keyword) || category.includes(keyword);
