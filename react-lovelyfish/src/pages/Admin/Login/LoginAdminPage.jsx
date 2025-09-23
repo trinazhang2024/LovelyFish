2b302654b81bd396262389df4aca81d6e7de3d56
Jinfeng-Zhang-1158659 <jinfengtrina@gmail.com>
2025-09-23 16:56:47 +1200

    LoginAdmin updated

--- react-lovelyfish/src/pages/Admin/Login/LoginAdminPage.jsx
+++ react-lovelyfish/src/pages/Admin/Login/LoginAdminPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import api from "../../../API/axios";
 import { useUser } from "../../../contexts/UserContext"; 
 import "./LoginAdminPage.css";
